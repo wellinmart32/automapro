a3889021b3f6ce48f765367f15b52fcb1f6b925c
wellinmart32 <wellinmart32@gmail.com>
2026-01-13 10:44:00 -0500

    Iniciando el proyecto

---
+++ automapro-backend/automapro-backend/src/main/java/com/automapro/backend/AutomaproBackendApplication.java
@@ -0,0 +1,13 @@
+package com.automapro.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaproBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaproBackendApplication.class, args);
+	}
+
+}
---
+++ automapro-backend/automapro-backend/src/test/java/com/automapro/backend/AutomaproBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automapro.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomaproBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
